2218bee229bf201968f550d2fe3d8b88551a6883
Jose Diego Cardona <TuCorreo>
2025-01-11 12:19:57 -0500

    Primer commit del proyecto

---
+++ PedidosForm.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace pedidosfarmaciaa
+{
+    partial class PedidosForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblMedicamento = new System.Windows.Forms.Label();
+            this.txtMedicamento = new System.Windows.Forms.TextBox();
+            this.lblTipoMedicamento = new System.Windows.Forms.Label();
+            this.cmbTipoMedicamento = new System.Windows.Forms.ComboBox();
+            this.lblDistribuidor = new System.Windows.Forms.Label();
+            this.rdbDistribuidor1 = new System.Windows.Forms.RadioButton();
+            this.rdbDistribuidor2 = new System.Windows.Forms.RadioButton();
+            this.rdbDistribuidor3 = new System.Windows.Forms.RadioButton();
+            this.lblSucursales = new System.Windows.Forms.Label();
+            this.chkSucursalPrincipal = new System.Windows.Forms.CheckBox();
+            this.chkSucursalSecundaria = new System.Windows.Forms.CheckBox();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMedicamento
+            // 
+            this.lblMedicamento.AutoSize = true;
+            this.lblMedicamento.Location = new System.Drawing.Point(36, 16);
+            this.lblMedicamento.Name = "lblMedicamento";
+            this.lblMedicamento.Size = new System.Drawing.Size(139, 16);
+            this.lblMedicamento.TabIndex = 0;
+            this.lblMedicamento.Text = "Nombre del medicamento:";
+            // 
+            // txtMedicamento
+            // 
+            this.txtMedicamento.Location = new System.Drawing.Point(36, 36);
+            this.txtMedicamento.Name = "txtMedicamento";
+            this.txtMedicamento.Size = new System.Drawing.Size(231, 22);
+            this.txtMedicamento.TabIndex = 1;
+            // 
+            // lblTipoMedicamento
+            // 
+            this.lblTipoMedicamento.AutoSize = true;
+            this.lblTipoMedicamento.Location = new System.Drawing.Point(36, 66);
+            this.lblTipoMedicamento.Name = "lblTipoMedicamento";
+            this.lblTipoMedicamento.Size = new System.Drawing.Size(120, 16);
+            this.lblTipoMedicamento.TabIndex = 2;
+            this.lblTipoMedicamento.Text = "Tipo de medicamento:";
+            // 
+            // cmbTipoMedicamento
+            // 
+            this.cmbTipoMedicamento.FormattingEnabled = true;
+            this.cmbTipoMedicamento.Items.AddRange(new object[]
+            {
+                "Seleccione...",
+                "Analgésico",
+                "Analéptico",
+                "Anestésico",
+                "Antiácido",
+                "Antidepresivo",
+                "Antibiótico"
+            });
+            this.cmbTipoMedicamento.Location = new System.Drawing.Point(36, 85);
+            this.cmbTipoMedicamento.Name = "cmbTipoMedicamento";
+            this.cmbTipoMedicamento.Size = new System.Drawing.Size(230, 24);
+            this.cmbTipoMedicamento.TabIndex = 3;
+            this.cmbTipoMedicamento.SelectedIndex = 0;
+            // 
+            // lblDistribuidor
+            // 
+            this.lblDistribuidor.AutoSize = true;
+            this.lblDistribuidor.Location = new System.Drawing.Point(36, 122);
+            this.lblDistribuidor.Name = "lblDistribuidor";
+            this.lblDistribuidor.Size = new System.Drawing.Size(76, 16);
+            this.lblDistribuidor.TabIndex = 4;
+            this.lblDistribuidor.Text = "Distribuidor:";
+            // 
+            // rdbDistribuidor1
+            // 
+            this.rdbDistribuidor1.AutoSize = true;
+            this.rdbDistribuidor1.Location = new System.Drawing.Point(36, 141);
+            this.rdbDistribuidor1.Name = "rdbDistribuidor1";
+            this.rdbDistribuidor1.Size = new System.Drawing.Size(87, 20);
+            this.rdbDistribuidor1.TabIndex = 5;
+            this.rdbDistribuidor1.TabStop = false; // Sin preselección
+            this.rdbDistribuidor1.Text = "Cofarma";
+            this.rdbDistribuidor1.UseVisualStyleBackColor = true;
+            // 
+            // rdbDistribuidor2
+            // 
+            this.rdbDistribuidor2.AutoSize = true;
+            this.rdbDistribuidor2.Location = new System.Drawing.Point(135, 141);
+            this.rdbDistribuidor2.Name = "rdbDistribuidor2";
+            this.rdbDistribuidor2.Size = new System.Drawing.Size(97, 20);
+            this.rdbDistribuidor2.TabIndex = 6;
+            this.rdbDistribuidor2.TabStop = false; // Sin preselección
+            this.rdbDistribuidor2.Text = "Empsephar";
+            this.rdbDistribuidor2.UseVisualStyleBackColor = true;
+            // 
+            // rdbDistribuidor3
+            // 
+            this.rdbDistribuidor3.AutoSize = true;
+            this.rdbDistribuidor3.Location = new System.Drawing.Point(245, 141);
+            this.rdbDistribuidor3.Name = "rdbDistribuidor3";
+            this.rdbDistribuidor3.Size = new System.Drawing.Size(76, 20);
+            this.rdbDistribuidor3.TabIndex = 7;
+            this.rdbDistribuidor3.TabStop = false; // Sin preselección
+            this.rdbDistribuidor3.Text = "Cemefar";
+            this.rdbDistribuidor3.UseVisualStyleBackColor = true;
+            // 
+            // lblSucursales
+            // 
+            this.lblSucursales.AutoSize = true;
+            this.lblSucursales.Location = new System.Drawing.Point(36, 174);
+            this.lblSucursales.Name = "lblSucursales";
+            this.lblSucursales.Size = new System.Drawing.Size(73, 16);
+            this.lblSucursales.TabIndex = 8;
+            this.lblSucursales.Text = "Sucursales:";
+            // 
+            // chkSucursalPrincipal
+            // 
+            this.chkSucursalPrincipal.AutoSize = true;
+            this.chkSucursalPrincipal.Location = new System.Drawing.Point(36, 193);
+            this.chkSucursalPrincipal.Name = "chkSucursalPrincipal";
+            this.chkSucursalPrincipal.Size = new System.Drawing.Size(146, 20);
+            this.chkSucursalPrincipal.TabIndex = 9;
+            this.chkSucursalPrincipal.Text = "Sucursal Principal";
+            this.chkSucursalPrincipal.UseVisualStyleBackColor = true;
+            // 
+            // chkSucursalSecundaria
+            // 
+            this.chkSucursalSecundaria.AutoSize = true;
+            this.chkSucursalSecundaria.Location = new System.Drawing.Point(190, 193);
+            this.chkSucursalSecundaria.Name = "chkSucursalSecundaria";
+            this.chkSucursalSecundaria.Size = new System.Drawing.Size(164, 20);
+            this.chkSucursalSecundaria.TabIndex = 10;
+            this.chkSucursalSecundaria.Text = "Sucursal Secundaria";
+            this.chkSucursalSecundaria.UseVisualStyleBackColor = true;
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(36, 223);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(63, 16);
+            this.lblCantidad.TabIndex = 11;
+            this.lblCantidad.Text = "Cantidad:";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(36, 242);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 22);
+            this.txtCantidad.TabIndex = 12;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(124, 286);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(156, 44);
+            this.btnConfirmar.TabIndex = 13;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.BtnConfirmar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(303, 282);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(155, 47);
+            this.btnCancelar.TabIndex = 14;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.BtnCancelar_Click);
+            // 
+            // PedidosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.chkSucursalSecundaria);
+            this.Controls.Add(this.chkSucursalPrincipal);
+            this.Controls.Add(this.lblSucursales);
+            this.Controls.Add(this.rdbDistribuidor3);
+            this.Controls.Add(this.rdbDistribuidor2);
+            this.Controls.Add(this.rdbDistribuidor1);
+            this.Controls.Add(this.lblDistribuidor);
+            this.Controls.Add(this.cmbTipoMedicamento);
+            this.Controls.Add(this.lblTipoMedicamento);
+            this.Controls.Add(this.txtMedicamento);
+            this.Controls.Add(this.lblMedicamento);
+            this.Name = "PedidosForm";
+            this.Text = "Sistema de Pedidos de Farmacia";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label lblMedicamento;
+        private System.Windows.Forms.TextBox txtMedicamento;
+        private System.Windows.Forms.Label lblTipoMedicamento;
+        private System.Windows.Forms.ComboBox cmbTipoMedicamento;
+        private System.Windows.Forms.Label lblDistribuidor;
+        private System.Windows.Forms.RadioButton rdbDistribuidor1;
+        private System.Windows.Forms.RadioButton rdbDistribuidor2;
+        private System.Windows.Forms.RadioButton rdbDistribuidor3;
+        private System.Windows.Forms.Label lblSucursales;
+        private System.Windows.Forms.CheckBox chkSucursalPrincipal;
+        private System.Windows.Forms.CheckBox chkSucursalSecundaria;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
---
+++ PedidosForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace pedidosfarmaciaa
+{
+    public partial class PedidosForm : Form
+    {
+        public PedidosForm()
+        {
+            InitializeComponent();
+
+            txtMedicamento.Text = "Nombre del medicamento";
+            txtMedicamento.ForeColor = System.Drawing.Color.Gray;
+            txtMedicamento.GotFocus += TxtMedicamento_GotFocus;
+            txtMedicamento.LostFocus += TxtMedicamento_LostFocus;
+        }
+
+        private void TxtMedicamento_GotFocus(object sender, EventArgs e)
+        {
+            if (txtMedicamento.Text == "Nombre del medicamento")
+            {
+                txtMedicamento.Text = string.Empty;
+                txtMedicamento.ForeColor = System.Drawing.Color.Black;
+            }
+        }
+
+        private void TxtMedicamento_LostFocus(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMedicamento.Text))
+            {
+                txtMedicamento.Text = "Nombre del medicamento";
+                txtMedicamento.ForeColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void BtnConfirmar_Click(object sender, EventArgs e)
+        {
+            if (txtMedicamento.Text == "Nombre del medicamento" || string.IsNullOrWhiteSpace(txtMedicamento.Text))
+            {
+                MessageBox.Show("Por favor, ingresa el nombre del medicamento.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (cmbTipoMedicamento.SelectedIndex == 0)
+            {
+                MessageBox.Show("Por favor, selecciona un tipo de medicamento válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!chkSucursalPrincipal.Checked && !chkSucursalSecundaria.Checked)
+            {
+                MessageBox.Show("Por favor, selecciona al menos una sucursal.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!int.TryParse(txtCantidad.Text, out int cantidad) || cantidad <= 0)
+            {
+                MessageBox.Show("Por favor, ingresa una cantidad válida mayor a 0.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string distribuidor = rdbDistribuidor1.Checked ? "Cofarma" :
+                                  rdbDistribuidor2.Checked ? "Empsephar" :
+                                  rdbDistribuidor3.Checked ? "Cemefar" : "Ninguno";
+
+            MessageBox.Show($"Pedido confirmado:\n" +
+                            $"- Medicamento: {txtMedicamento.Text}\n" +
+                            $"- Tipo: {cmbTipoMedicamento.SelectedItem}\n" +
+                            $"- Distribuidor: {distribuidor}\n" +
+                            $"- Cantidad: {cantidad}\n" +
+                            $"- Sucursales: {(chkSucursalPrincipal.Checked ? "Principal " : "")}{(chkSucursalSecundaria.Checked ? "Secundaria" : "")}");
+        }
+
+        private void BtnCancelar_Click(object sender, EventArgs e)
+        {
+            txtMedicamento.Text = "Nombre del medicamento";
+            txtMedicamento.ForeColor = System.Drawing.Color.Gray;
+            cmbTipoMedicamento.SelectedIndex = 0;
+            rdbDistribuidor1.Checked = false;
+            rdbDistribuidor2.Checked = false;
+            rdbDistribuidor3.Checked = false;
+            chkSucursalPrincipal.Checked = false;
+            chkSucursalSecundaria.Checked = false;
+            txtCantidad.Clear();
+
+            MessageBox.Show("Formulario reiniciado.");
+        }
+    }
+}
